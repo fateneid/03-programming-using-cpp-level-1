2c6345e050294bcc4a9a65280725ef7eb7feb62e
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 03:08:15 +0300

    remove unnecessary lines and comments

--- 03-programming-using-cpp-level-1.cpp
+++ 03-programming-using-cpp-level-1.cpp
@@ -1,28 +1,13 @@
 #include <iostream>
-#include "L12_Output_Print/L12_1.h"
 #include "L20_ArithmeticOperators/L20_P15.h"
 
 using namespace std;
 
 int main()
 {
-
-	L12_1::Task();
-    L20_P15::Task();
+	
+	L20_P15::Task();
 
 }
 
 // in L36 uncomment the global x line to make code work
-
-
-/*
-
-library's namespace name = library name
-
-To run any problem:
-
-    - include its library with the right path if its inside folder 
-    - use its namespace with any method
-    - run Task fun under its namespace
-
-*/
